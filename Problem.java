2f8198a313a7d3170686cd87c312a821a8567f9e
shilppatel913 <shilppatel77@gmail.com>
2022-12-16 15:05:24 +0530

    minor change

--- Problem.java
+++ Problem.java
@@ -14,7 +14,8 @@ public class Problem {
 	}
 
 	private static boolean isPrime(int n) {
-		if(n==1 || n==0 || n==2) return false;
+		if(n==1 || n==0) return false;
+		if(n==2) return true;
 		
 		for(int i=2;i<n;i++) {
 			if(n%i==0) return false;
